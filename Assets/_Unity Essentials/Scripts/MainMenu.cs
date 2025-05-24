3cc8063a839fc87c7231b8fc6100563ab464186f
Taiwo <139594926+Taiwo1105@users.noreply.github.com>
2025-05-24 13:51:07 +0200

    unity ui

--- Assets/_Unity Essentials/Scripts/MainMenu.cs
+++ Assets/_Unity Essentials/Scripts/MainMenu.cs
@@ -6,6 +6,20 @@ public class MainMenu : MonoBehaviour
     // Call this method from a UI button to start the Maze scene
     public void PlayMaze()
     {
-        SceneManager.LoadScene("maze"); // Replace "Maze" with your actual scene name
+        SceneManager.LoadScene("Maze"); // Replace "Maze" with your actual scene name
+    }
+
+    // Call this method from a UI button to quit the game
+    public void QuitMaze()
+    {
+        Debug.Log("Quit Game");
+
+        // Quit the application (works in built game)
+        Application.Quit();
+
+        // Stop play mode in Unity Editor (does not quit the editor)
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
