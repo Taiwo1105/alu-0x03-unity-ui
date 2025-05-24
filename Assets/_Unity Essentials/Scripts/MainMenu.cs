c72e0e3dd0a8d192b3c0c11b71614fbcb936a21d
Taiwo <139594926+Taiwo1105@users.noreply.github.com>
2025-05-24 16:13:48 +0200

    unity ui

--- Assets/_Unity Essentials/Scripts/MainMenu.cs
+++ Assets/_Unity Essentials/Scripts/MainMenu.cs
@@ -1,25 +1,38 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Material trapMat;           // Material used for Traps
+    public Material goalMat;           // Material used for Goal
+    public Toggle colorblindMode;      // Toggle for colorblind mode
+
     // Call this method from a UI button to start the Maze scene
     public void PlayMaze()
     {
-        SceneManager.LoadScene("Maze"); // Replace "Maze" with your actual scene name
+        // Check if colorblind mode is enabled
+        if (colorblindMode != null && colorblindMode.isOn)
+        {
+            if (trapMat != null)
+            {
+                trapMat.color = new Color32(255, 112, 0, 255); // Orange
+            }
+
+            if (goalMat != null)
+            {
+                goalMat.color = Color.blue; // Blue
+            }
+        }
+
+        SceneManager.LoadScene("Maze"); // Load the Maze scene
     }
 
     // Call this method from a UI button to quit the game
     public void QuitMaze()
     {
         Debug.Log("Quit Game");
-
-        // Quit the application (works in built game)
         Application.Quit();
-
-        // Stop play mode in Unity Editor (does not quit the editor)
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#endif
     }
 }
+
